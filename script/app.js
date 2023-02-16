565915044cbb8f6321e64db4834cefda3c768a93
salawudeen abdultawab <abdultawabsalawudeen@gmail.com>
2023-02-16 13:25:57 +0100

    scroll update

--- script/app.js
+++ script/app.js
@@ -12,14 +12,54 @@ function getElement(selection){
 }
 
 const navToggle = getElement('.nav-toggle');
-const sidebar = getElement('.overlay')
-const closeBtn = getElement('.close-btn')
+const sidebar = getElement('.overlay');
+const closeBtn = getElement('.close-btn');
+const scrollLink = getElement('.nav-links');
+const navBar = getElement('.nav-bar');
+const mobileMenu = getElement('.mobile-menu');
 
 navToggle.addEventListener('click', ()=>{
   sidebar.classList.add('show');
 })
 
 closeBtn.addEventListener('click', ()=>{
-  sidebar.classList.remove('show')
+  sidebar.classList.remove('show');
 })
+
+scrollLink.addEventListener('click', (e)=>{
+  e.preventDefault();
+  let targetElement = e.target;
+  let targetParent = targetElement.parentElement;
+  const navHeight = navBar.getBoundingClientRect().height;
+  if(targetParent.classList.contains('scroll-link')){
+    scrollFunction(targetElement, navHeight);
+  }
+})
+
+mobileMenu.addEventListener('click', (e)=>{
+  e.preventDefault();
+  let targetElement = e.target;
+  const navHeight = navBar.getBoundingClientRect().height;
+  sidebar.classList.remove('show');
+  if(targetElement.classList.contains('scroll-link')){
+    scrollFunction(targetElement, navHeight);
+  }
+  
+})
+
+function scrollFunction (targetElement, navHeight){
+  let id;
+  if(targetElement.classList.contains('fa-solid')){
+      id = targetElement.parentElement.getAttribute('href').slice(1);
+    } else {
+      id = targetElement.getAttribute('href').slice(1);
+    }
+    const element = document.getElementById(id);
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+}
 
